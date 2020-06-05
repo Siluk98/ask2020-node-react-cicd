230e2afba99023cf33368ad0d376d72114de9e5e
Arkadiusz Kuliś <arkadiuszokulis@gmail.com>
2020-06-05 19:45:14 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -13,6 +13,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 //pls run me circle
+//run me baby one more time
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

23200579125c7f4546c4f187b57c7b124da425ad
Arkadiusz Kuliś <arkadiuszokulis@gmail.com>
2020-06-05 19:40:37 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+//pls run me circle
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
